13dea0c2c4d295def2997cf91a8cd9f2c34031f0
alexwhyhey <rystambaybakov@gmail.com>
2025-04-28 21:40:43 +0500

    serializers and models

--- ufanet/partners/serializers.py
+++ ufanet/partners/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import *
-from django.core.validators import FileExtensionValidator
+from django.core.validators import FileExtensionValidator, RegexValidator
 
 class CategorySerializer(serializers.ModelSerializer):
     logo = serializers.ImageField(
@@ -55,14 +55,22 @@ class PartnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'title', 'logo', 'about']
+        read_only_fields = ['id']
 
     def validate_logo(self, file):
         if file and file.size > 2 * 1024 * 1024:
             raise serializers.ValidationError("Image too large")
         return file
 
-class OfferSerializer(serializers.ModelSerializer):
 
+class OfferSerializer(serializers.ModelSerializer):
+    partner = PartnerSerializer(read_only=True)
+
+    partner_id = serializers.PrimaryKeyRelatedField(
+        queryset=Partner.objects.all(),
+        source='partner',
+        write_only=True  
+    )
     
     back_image = serializers.ImageField(
         required=False,
@@ -76,9 +84,60 @@ class OfferSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ['id', 'partner', 'city', 'button_name', 'url', 'what_offer_about', 'where_to_use', 'back_image', 'how_to_get', 'start_date', 'end_date']
+        fields = ['id', 'partner', 'partner_id', 'city', 'button_name', 'url', 'what_offer_about', 'where_to_use', 'back_image', 'how_to_get', 'start_date', 'end_date']
+        read_only_fields = ['id']
 
     def validate_logo(self, file):
         if file and file.size > 2 * 1024 * 1024:
             raise serializers.ValidationError("Image too large")
         return file
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+    
+
+class OfferTagSerializer(serializers.ModelSerializer):
+    offer = OfferSerializer(read_only=True)
+    tag = TagSerializer(read_only=True)
+
+    offer_id = serializers.PrimaryKeyRelatedField(
+        queryset=Offer.objects.all(),
+        source='offer',
+        write_only=True
+    )
+    tag_id = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        source='tag',
+        write_only=True
+    )
+
+    class Meta:
+        model = OfferTag
+        fields = ['id', 'offer', 'tag']
+        read_only_fields = ['id']
+
+
+class CategoryTagSerialzier(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    tag = TagSerializer(read_only=True)
+
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        source='category',
+        write_only=True
+    )
+    tag_id = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        source='tag',
+        write_only=True
+    )
+
+    class Meta:
+        model = CategoryTag
+        fields = ['id', 'category', 'category_id', 'tag', 'tag_id']
+        read_only_fields = ['id']
+
