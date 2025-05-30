cc6b1b747cacbbf4503e775ee4fb162d66ae1660
alexwhyhey <rystambaybakov@gmail.com>
2025-05-30 21:56:05 +0500

    kafka, rebuild architecture

--- ufanet/ufanet/settings.py
+++ ufanet/ufanet/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '../.env')
+load_dotenv(BASE_DIR / './.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = getenv('SECRET_KEY')
